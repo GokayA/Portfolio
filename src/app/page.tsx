f2806409110c4a5441faf9a5fdac4c65ea87e9dc
yokyaa <arjenrobbennn@gmail.com>
2023-05-16 15:08:56 +0300

    added homepage image and name,title page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { aboutMeData } from './data/data';
 
 export default function Home() {
   return (
-  <main>
-    Hello
-  </main>
-  )
+    <main className="flex flex-col min-h-screen">
+      <div className="flex  items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="text-5xl text-white font-bold mb-4 ">
+            {aboutMeData.name}
+          </h1>
+          <p className="text-xl 0 mb-8">{aboutMeData.title}</p>
+        </div>
+        <Image
+          width={2500}
+          height={2000}
+          className="h-full w-full object-cover opacity-40 absolute"
+          src="/background.jpg"
+          alt="Background Image"
+        />
+      </div>
+    </main>
+  );
 }

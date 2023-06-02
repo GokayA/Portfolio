4ea525d35ef0bd5b4acadc3ea2899c4f1e076026
yokyaa <arjenrobbennn@gmail.com>
2023-06-02 15:32:49 +0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 import { Montserrat } from 'next/font/google';
+
 import './globals.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -20,6 +22,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
